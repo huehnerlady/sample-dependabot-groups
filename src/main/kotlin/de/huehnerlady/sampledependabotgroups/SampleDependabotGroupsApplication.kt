d44667f72982c5afee64e3ca9ab23af124937e96
Ruth Bassindale <ruth.bassindale@europace.de>
2023-08-21 14:12:34 +0200

    Add sample gradle project

---
+++ src/main/kotlin/de/huehnerlady/sampledependabotgroups/SampleDependabotGroupsApplication.kt
@@ -0,0 +1,11 @@
+package de.huehnerlady.sampledependabotgroups
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SampleDependabotGroupsApplication
+
+fun main(args: Array<String>) {
+  runApplication<SampleDependabotGroupsApplication>(*args)
+}
---
+++ src/test/kotlin/de/huehnerlady/sampledependabotgroups/SampleDependabotGroupsApplicationTests.kt
@@ -0,0 +1,12 @@
+package de.huehnerlady.sampledependabotgroups
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SampleDependabotGroupsApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+}
